3acc8f4010a91ea3674d65879fc922ac3db422d5
rex.chien <rex.chien.0509@gmail.com>
2017-08-27 22:50:05 +0800

    Production code for Scenario 2.

--- PotterShopCart/ShoppingCart.cs
+++ PotterShopCart/ShoppingCart.cs
@@ -10,9 +10,14 @@ namespace PotterShopCart
 
         public int CalculatePotterPrice(int[] buyedPotterSeries)
         {
-            var totalPrice = buyedPotterSeries.Sum() * 100;
+            var totalPrice = buyedPotterSeries.Sum() * 100.0;
 
-            return totalPrice;
+            if (buyedPotterSeries.Where(amount => amount > 0).Count() > 1)
+            {
+                totalPrice *= 0.95;
+            }
+
+            return (int) totalPrice;
         }
     }
 }
