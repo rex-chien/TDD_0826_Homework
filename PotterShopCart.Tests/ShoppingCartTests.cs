20b23a206aef6402c4247797439fcda04a2e8b9f
rex.chien <rex.chien.0509@gmail.com>
2017-08-30 10:34:56 +0800

    Test case for Scenario 3.

--- PotterShopCart.Tests/ShoppingCartTests.cs
+++ PotterShopCart.Tests/ShoppingCartTests.cs
@@ -7,36 +7,52 @@ using System.Text;
 
 namespace PotterShopCart.Tests
 {
-    [TestClass()]
+    [TestClass]
     public class ShoppingCartTests
     {
-        [TestMethod()]
+        [TestMethod]
         public void CalculatePriceTest_五集買的本數_1_0_0_0_0_總價_100()
         {
             // arrange
             var shoppingCart = new ShoppingCart();
-            var buiedPotterSeries = new[] { 1, 0, 0, 0, 0 };
+            var buyedPotterSeries = new[] { 1, 0, 0, 0, 0 };
 
             var expected = 100;
 
             // act
-            var actual = shoppingCart.CalculatePotterPrice(buiedPotterSeries);
+            var actual = shoppingCart.CalculatePotterPrice(buyedPotterSeries);
 
             // assert
             Assert.AreEqual(expected, actual);
         }
 
-        [TestMethod()]
+        [TestMethod]
         public void CalculatePriceTest_五集買的本數_1_1_0_0_0_總價_190()
         {
             // arrange
             var shoppingCart = new ShoppingCart();
-            var buiedPotterSeries = new[] { 1, 1, 0, 0, 0 };
+            var buyedPotterSeries = new[] { 1, 1, 0, 0, 0 };
 
             var expected = 190;
 
             // act
-            var actual = shoppingCart.CalculatePotterPrice(buiedPotterSeries);
+            var actual = shoppingCart.CalculatePotterPrice(buyedPotterSeries);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalculatePriceTest_五集買的本數_1_1_1_0_0_總價_270()
+        {
+            // arrange
+            var shoppingCart = new ShoppingCart();
+            var buyedPotterSeries = new[] { 1, 1, 1, 0, 0 };
+
+            var expected = 270;
+
+            // act
+            var actual = shoppingCart.CalculatePotterPrice(buyedPotterSeries);
 
             // assert
             Assert.AreEqual(expected, actual);
